7e81e3803fdb91d5ad1df5f842839ceb613c945e
Abhishek Shukla <abhishekshuklaratedr@gmail.com>
2020-07-29 08:14:48 +0530

    Routes set up

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 let express = require("express");
 let app = express();
 let bodyParser = require("body-parser");
-let mongoose = require("mongoose");
+
+let mongoose = require("mongoose"),
+  passport = require("passport"),
+  LocalStrategy = require("passport-local"),
+  passportLocalMongoose = require("passport-local-mongoose");
+
 app.set("view engine", "ejs");
 const jsdom = require("jsdom");
 const { JSDOM } = jsdom;
@@ -20,8 +25,23 @@ let userSchema = new mongoose.Schema({
   password: String,
 });
 
+userSchema.plugin(passportLocalMongoose);
 let User = mongoose.model("User", userSchema);
 
+//Passport Setup
+app.use(
+  require("express-session")({
+    secret: "Booketeer",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 //Rest Routes
 app.get("/", (req, res) => {
   res.render("landing");
@@ -39,20 +59,34 @@ app.get("/register", (req, res) => {
 
 //Sign up logic
 app.post("/register", (req, res) => {
-  User.create(req.body.user, function (err, createdUser) {
-    if (err) console.log("ERROR:- " + err);
-    else {
-      console.log(createdUser);
-      res.redirect("/");
+  let newUser = new User({ username: req.body.username, age: req.body.age });
+  User.register(newUser, req.body.password, function (err, user) {
+    if (err) {
+      console.log("ERROR:- " + err);
+      res.redirect("/register");
+    } else {
+      passport.authenticate("local")(req, res, function () {
+        res.redirect("/book");
+      });
     }
   });
 });
 
-//Loggin in an existing user
+//Logging in an existing user
 //Displaying the login form
 app.get("/login", (req, res) => {
   res.render("login");
 });
+
+//Login Logic
+app.post(
+  "/login",
+  passport.authenticate("local", {
+    successRedirect: "/book",
+    failureRedirect: "/",
+  }),
+  (req, res) => {}
+);
 
 app.listen(3000, (req, res) => {
   console.log("Booketeer running on port 3000!");
